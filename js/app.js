e458f0c985aaceda52d2b157d6b3a8441b9840dd
Isaac Abraham <Isaac Abraham>
2016-11-12 00:16:28 +0530

    Have rendered the characters, need to fix gamelife

--- js/app.js
+++ js/app.js
@@ -1,36 +1,179 @@
+var enemyPosY = [60, 143, 226];
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  
+  this.x = -100;
+  this.y = enemyPosY[Math.floor(Math.random() * 3)];
+  this.speed = getRandomInt(100, 550);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
+  if(this.x > 6 * 101) {
+    this.x = -101;
+    this.speed = getRandomInt(100, 550);
+  }
+
+  // Handle collisions with the player
+  if (
+        player.y + 131 >= this.y + 90
+        && player.x + 25 <= this.x + 88
+        && player.y + 73 <= this.y + 135
+        && player.x + 76 >= this.x + 11) { 
+    player.reset();
+    score.updateMiss();
+    gameLife.decrease();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.reset();
+};
+
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+  if(this.col < 0) {
+    this.col = 0;
+  }
+
+  if(this.col > 4) {
+    this.col = 4;
+  }
+
+  if(this.row > 5) {
+    this.row = 5;
+  }
+
+  // Reset the player's position... has reached the water
+  if(this.row == 0) {
+    this.reset();
+    score.updateSuccess();
+  }
+
+  this.x = this.col * 101;
+  this.y = this.row * 83;
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Handle user input for controlling the player
+Player.prototype.handleInput = function(key) {
+  switch(key) {
+    case 'left':
+      this.col--;
+      break;
+    case 'right':
+      this.col++;
+      break;
+    case 'up':
+      this.row--;
+      break;
+    case 'down':
+      this.row++;
+      break;
+  }
+};
+
+Player.prototype.reset = function() {
+  this.col = 2;
+  this.row = 5;
+  this.x = this.col * 101;
+  this.y = this.row * 83;
+};
+
+var Score = function() {
+  this.success = 0;
+  this.miss = 0;
+};
+
+Score.prototype.updateSuccess = function() {
+  this.success += 1;
+  document.getElementById('score-success').innerHTML = this.success;
+  gameLevel += 1;
+  console.log('current score: ' + score + ', current level: ' + gameLevel);
+  
+    allEnemies.push(new Enemy());
+  
+};
+
+Score.prototype.updateMiss = function() {
+  this.miss += 1;
+  document.getElementById('score-miss').innerHTML = this.miss;
+};
+
+Score.prototype.reset = function(){
+  this.success = 0;
+  this.success = 0;
+};
+
+var Life = function() {
+  this.lifeImg = 'images/Heart.png';
+  this.life = 3;
+}
+/**
+ * Renders the life on the screen.
+ */
+Life.prototype.render = function() {
+  var x = 0;
+  for (var i = 0; i < this.life; i++) {
+    ctx.drawImage(Resources.get(this.lifeImg), x, 630);
+    x = x + 50;
+  }
+  if (this.life === 1) {
+    ctx.drawImage(Resources.get('images/GameOver.png'), 0, 50);
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'white';
+    ctx.fillText("You're Score is"+score.success+" gems",300,420);
+  }
+}
+/**
+ * Decrease number of lives.
+ */
+Life.prototype.decrease = function() {
+  if (this.life > 0) {
+    this.life = this.life - 1;
+    console.log(this.life);
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var numEnemies = 3;
+var allEnemies = [];
+for(var i = 0; i < numEnemies; i++) {
+  allEnemies.push(new Enemy());
+}
+var player = new Player();
+var gameLevel = 1;
+var score = new Score();
+var gameLife = new Life();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +187,7 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function getRandomInt(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+};
--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 505;
-    canvas.height = 606;
+    canvas.height = 650;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -171,7 +171,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Heart.png',
+        'images/GameOver.png'
     ]);
     Resources.onReady(init);
 
